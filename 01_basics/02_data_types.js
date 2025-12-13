f5f56d1473c0d97c874b5baa6625f6f1797e11a3
Amber <amber24march@gmail.com>
2025-12-13 12:42:25 +0530

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version.
+
+// alert(3+3) // we are using nodejs not browser .. this will only work there
+
+// datatypes
+
+//Primitive datatypes:
+
+//number->2 to power 53 range
+//bigint->for bigger numbes used in stock softwares
+//string->""
+//boolean->true false
+//null->standalone value .. empty 
+//undefined->not assigned value.
+//symbol->for uniqueness
+
+// object data types:->talk about later
+
+let Name="ars";
+console.log(typeof Name);
+console.log(typeof undefined);//answer->undefined
+console.log(typeof null);//answer->object
+
