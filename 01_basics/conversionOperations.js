c60d03ee72c4eb362eb16c5f7f9a6f7bd250ab96
Amber <amber24march@gmail.com>
2025-12-13 13:30:27 +0530

    conversion of datatype

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,37 @@
+//Number:
+let score="35abceed";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//"33"-->33
+//"35abceed"-->NaN(not a number but type is number).
+//true-->1
+//false-->0
+
+
+//Boolean:
+let islogged=1;
+
+let isloggedBoolean=Boolean(islogged);
+
+// console.log(isloggedBoolean);
+// console.log(typeof isloggedBoolean);
+
+//1-->true;
+//0-->false;
+//""-->false;
+//"ars"-->true;
+
+//String:
+let check=45;
+let isCheckString=String(check);
+
+console.log(isCheckString);
+console.log(typeof isCheckString);
+
+//45-->"45"
