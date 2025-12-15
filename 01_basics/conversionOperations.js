d6b0612586d6b84549c96ed039e07dd34b0003cc
amber <amber24march@gmail.com>
2025-12-15 20:46:20 +0530

    more

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -54,4 +54,15 @@ let negval=-value;
 let str1="hello";
 let str2=" love";
 let str3=str1+str2;
-console.log(str3);
+// console.log(str3);
+
+// console.log(1+"2");//12
+// console.log("1"+2);//12
+// console.log("1"+2+3);//123
+// console.log((2+"3")*5%2);//1
+
+console.log(true);//true
+console.log(+true);//1
+console.log(+"");//0
+// console.log(true+);//error
+
