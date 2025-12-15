02d0c1634727be5e54218bea30597d85de382e0a
amber <amber24march@gmail.com>
2025-12-15 21:19:46 +0530

    comparsion

---
+++ 01_basics/comparsion.js
@@ -0,0 +1,14 @@
+// console.log("2">1);//true
+// console.log("02">1);//true
+// //note:always use same type of datatypes in comparsion .. js does,nt stop you but typescript will 
+
+// console.log(null>0);//false
+// console.log(null==0);//false
+// console.log(null>=0);//true
+
+// console.log(undefined>0);//false
+// console.log(undefined==0);//false
+// console.log(undefined<0);//false
+
+// ===  -->check datatypes with value also 
+console.log("2"===2);
