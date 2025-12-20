3b9439747c116d08a154984313008ba10cd52089
AMBER RAJ SAXENA <amber24march@gmail.com>
2025-12-20 20:25:23 +0530

    datatypesummary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+//primitive:
+// 7 types :string ,number,Boolean,null,undefined,symbol,BigInt
+
+//Reference or (non Primitive):
+//Array,objects,Functions
+
+//JS is dyanmically typed and lossesly typed .. 
+// dynamic because any variable can hold any dataype and losse 
+// because it allows many things which cause error like "5"+1 ;
+
+const heros=["shaktiman","ironman","wonderwomen"];//array
+
+let my_obj={
+    name:"amber",
+    age:22,
+}//objects
+
+const myfunction=function(){
+    console.log("hello world");
+}//function
+
+
